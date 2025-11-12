e3c45041485147ecfedc9e5ee4760be70967104e
Mara Kim <mara@autochthe.net>
2025-11-12 18:46:28 -0500

    setup app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,18 @@ import { PixiThreeCanvas } from "./components/pixi-three-canvas";
 
 function App() {
   return (
-    <>
-      <PixiThreeCanvas className="h-full w-full"></PixiThreeCanvas>
-    </>
+    <div className="m-4">
+      <h1 className="text-xl">Pixi + Three example</h1>
+      <ul className="list-disc ml-4">
+        <li>Three.js is rendering a grey cube.</li>
+        <li>Pixi.js is rendering a red square.</li>
+        <li>
+          Canvas container has tailwind class{" "}
+          <code className="bg-gray-300">h-[30lvh] w-[30lvw]</code>
+        </li>
+      </ul>
+      <PixiThreeCanvas className="h-[30lvh] w-[30lvw]"></PixiThreeCanvas>
+    </div>
   );
 }
 
